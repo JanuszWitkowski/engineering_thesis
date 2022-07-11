d5777c047af72c396f740deedd81c9d9eca265f2
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2022-07-11 23:44:52 +0200

    Outline heuristic params

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         // initial call
-//        minimax(state, 3, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        int depth = 3;
+//        Heuristic heuristic = new Heuristic();
+//        heuristic.generateParamValues();
+//        minimax(state, depth, heuristic, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
     }
 }
--- src/MinMax.java
+++ src/MinMax.java
@@ -1,15 +1,15 @@
 public class MinMax {
 
-//    public static int minimax(State state, int depth, int alpha, int beta, boolean maximizingPlayer) {
+//    public static int minimax(State state, int depth, Heuristic heuristic, int alpha, int beta, boolean maximizingPlayer) {
 //        if (depth == 0 || state.gameOver()) {
-//            return static evaluation of state;
+//            return heuristic.evaluate(state);
 //        }
 //
 //        if (maximizingPlayer) {
 //            int maxEval = Integer.MIN_VALUE;
 //            for (State child : state.getChildren()) {
 //                int eval = minimax(child, depth - 1, alpha, beta, false);
-//                if (maxEval < eval) maxEval = eval;
+//                if (maxEval < eval) maxEval = eval;   // TODO: wprowadź element losowy (wylosuj z grupy najlepszych o tej samej wartości oceny)
 //                if (alpha < eval) alpha = eval;
 //                if (beta <= alpha) break;
 //            }
