00e1a48b19637f7a71ef23c3f12675dab8861807
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2022-09-18 19:18:57 +0200

    Start implementing Play interface

--- src/Test.java
+++ src/Test.java
@@ -301,6 +301,8 @@ public class Test {
     }
 
     public static void main (String[] args) {
+        System.out.println(Console.BLUE_BOLD + "#### TESTING ####" + Console.RESET);
+        System.out.println(Console.BLUE_BOLD + "# Zbiór testowy #" + Console.RESET);
 //        testAIvsAI(5, 5, 10);
 //        testHumanVsAI();
 //        testOneParamInGame();
